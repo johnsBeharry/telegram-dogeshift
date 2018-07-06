b65a584904cf5cd5f8cd17574869a38a649e4d39
micey969 <micey_969@hotmail.com>
2018-07-06 13:08:38 -0400

    chore(setup): get branch setup locally on my machine

---
+++ features/steps/steps.py
@@ -0,0 +1,16 @@
+from behave import * 
+
+
+@given(u'a user is registered')
+def get_user(context):
+    
+
+
+@when(u'they type "{command}"')
+def process(context, command):
+    
+
+
+@then(u'their balance in doge coin will be returned')
+def return_bal(context):
+    
---
+++ features/telegram-bot.feature
@@ -0,0 +1,13 @@
+Feature: Telegram Bot
+As a ___ 
+I want ___
+So that ___
+
+
+
+Scenario: Get user balance
+Given a user is registered
+When they type "/balance"
+Then their balance in doge coin will be returned
+
+
