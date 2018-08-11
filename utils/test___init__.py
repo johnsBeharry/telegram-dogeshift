a97a96d235f653451eb0489b9bc68e436481a8e9
vindard <vinda.rd@gmail.com>
2018-08-11 22:51:00 +0700

    test(msg_parse): add tests

---
+++ utils/test___init__.py
@@ -0,0 +1,71 @@
+import unittest
+from __init__ import msg_parse
+
+
+class TestMsgParse(unittest.TestCase):
+
+    def setUp(self):
+        self.monikers_flat = ["coffee", "coffees", 7]
+
+    def test_mixedOrder(self):
+        # Test that mixed order keywords get assigned properly
+        message = "/tip hey can u send 1 coffee across to my boy @username please :)"
+        message = msg_parse(message, self.monikers_flat)
+        self.assertEqual(message, [
+            "/tip",  # bot call
+            "username",  # username
+            "1",  # amount
+            "coffee",  # moniker
+            ""  # address
+        ])
+
+    def test_numberPlacement(self):
+        # Test that function can find the moniker when
+        #  it does not come after the digit
+        message = "/tip hey can u send some coffee across to my boy @username... 2 cups please :)"
+        message = msg_parse(message, self.monikers_flat)
+        self.assertEqual(message, [
+            "/tip",  # bot call
+            "username",  # username
+            "2",  # amount
+            "coffee",  # moniker
+            ""  # address
+        ])
+
+
+        # Test that function can parse a rounded down
+        # integer when a floating point number is passed
+        message = "/tip hey can u send some coffee across to my boy @username... 2.99 cups please :)"
+        message = msg_parse(message, self.monikers_flat)
+        self.assertEqual(message, [
+            "/tip",  # bot call
+            "username",  # username
+            "2",  # amount
+            "coffee",  # moniker
+            ""  # address
+        ])
+
+    def test_amountSingle(self):
+        # Test that function can assign an amount '1' when
+        # no amount is specified but moniker present
+        message = "/tip yo send coffee to @username"
+        message = msg_parse(message, self.monikers_flat)
+        self.assertEqual(message, [
+            "/tip",  # bot call
+            "username",  # username
+            "",  # amount
+            "coffee",  # moniker
+            ""  # address
+        ])
+
+        # Test that function can assign an amount '1' when
+        #  'a or an' amount classifier is used and moniker present
+        message = "/tip yo send a cup of coffee to @username"
+        message = msg_parse(message, self.monikers_flat)
+        self.assertEqual(message, [
+            "/tip",  # bot call
+            "username",  # username
+            "a",  # amount
+            "coffee",  # moniker
+            ""  # address
+        ])
